9ec8c5191d5235cd1e3b8c3d5282d9b46f439277
JosiahRininger <JosiahRininger@gmail.com>
2020-06-21 09:17:51 -0500

    Initial Commit

---
+++ Truth or Truth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Truth or Truth
+//
+//  Created by Josiah Rininger on 6/21/20.
+//  Copyright © 2020 Josiah Rininger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
